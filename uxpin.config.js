a26f5b29021cb97ec14b6b755d7b4a4d64beff5a
s.osugi <s.oosugi@linkwiz.co.jp>
2023-10-10 11:55:03 +0900

    Uxpinの設定追加した

---
+++ uxpin.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  components: {
+    categories: [
+      {
+        name: "General",
+        include: ["src/components/Form.js"],
+      },
+    ],
+    webpackConfig: "uxpin.webpack.config.js",
+  },
+  name: "Learn UXPin Merge - React Todo list tutorial",
+};
